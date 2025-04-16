427d3f32c5dea31161dace36e252f994bb276ec7
Bruno Pimentel <bpimente@redhat.com>
2025-04-16 15:51:04 -0300

    Turn the mount paths into variables
    This brings more flexibility to change the previously hard-coded values.

--- clone-and-build.sh
+++ clone-and-build.sh
@@ -4,6 +4,8 @@ source ${1:-input.env}
 
 SCRIPT_DIR=$(pwd)
 TMP_DIR=$(mktemp -d "/tmp/cachi2.play.XXXXXXXXXX")
+OUTPUT_MOUNT_DIR=/tmp/output
+SOURCE_MOUNT_DIR=/tmp/sources
 
 set -ex
 
@@ -19,35 +21,33 @@ mkdir output
 
 # prefetch dependencies
 podman run --rm \
-	-v $(realpath ./sources):/tmp/sources:z \
-	-v $(realpath ./output):/tmp/output:z \
+	-v $(realpath ./sources):"$SOURCE_MOUNT_DIR":z \
+	-v $(realpath ./output):"$OUTPUT_MOUNT_DIR":z \
 	"$CACHI2_IMAGE" \
 	--log-level "DEBUG" \
 	fetch-deps "$PREFETCH_INPUT" \
-	--source "/tmp/sources" \
-	--output "/tmp/output" \
+	--source "$SOURCE_MOUNT_DIR" \
+	--output "$OUTPUT_MOUNT_DIR" \
 	--dev-package-managers
 
 # generate environmnent variables
 podman run --rm \
-	-v $(realpath ./sources):/tmp/sources:z \
-	-v $(realpath ./output):/tmp/output:z \
+	-v $(realpath ./sources):"$SOURCE_MOUNT_DIR":z \
+	-v $(realpath ./output):"$OUTPUT_MOUNT_DIR":z \
 	"$CACHI2_IMAGE" \
-	generate-env /tmp/output \
+	generate-env "$OUTPUT_MOUNT_DIR" \
 	--format env \
-	--output /tmp/output/cachi2.env
-
-mv ./output/cachi2.env .
+	--output "$OUTPUT_MOUNT_DIR/cachi2.env"
 
 # inject project files
 podman run --rm \
-	-v $(realpath ./sources):/tmp/sources:z \
-	-v $(realpath ./output):/tmp/output:z \
+	-v $(realpath ./sources):"$SOURCE_MOUNT_DIR":z \
+	-v $(realpath ./output):"$OUTPUT_MOUNT_DIR":z \
 	"$CACHI2_IMAGE" \
-	inject-files /tmp/output
+	inject-files "$OUTPUT_MOUNT_DIR"
 
 # use the cachi2 env variables in all RUN instructions in the Containerfile
-sed -i 's|^\s*run |RUN . /tmp/cachi2.env \&\& \\\n    |i' "./sources/$CONTAINERFILE_PATH"
+sed -i "s|^\s*run |RUN . $OUTPUT_MOUNT_DIR/cachi2.env \&\& |i" "./sources/$CONTAINERFILE_PATH"
 
 # in case RPMs for x86_64 were prefetched, mount the repofiles during the container build
 if [ -d "./output/deps/rpm/x86_64/repos.d" ]; then
@@ -57,10 +57,8 @@ fi
 
 # build hermetically
 podman build -t "$OUTPUT_IMAGE" \
-	-v $(realpath ./output):/tmp/output:Z \
-	-v $(realpath ./cachi2.env):/tmp/cachi2.env \
+	-v $(realpath ./output):"$OUTPUT_MOUNT_DIR":z \
 	$MOUNT_RPM_REPOS \
-	--no-cache \
 	--network=none \
 	-f "./sources/$CONTAINERFILE_PATH" \
 	sources
